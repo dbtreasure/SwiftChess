5a109c3fad8ef826f6e4cf4c65ce8ce76b4931e0
Dan Treasure <beepboop3762@protonmail.com>
2022-05-17 22:39:17 -0600

    Initial Commit

---
+++ SwiftChess/SwiftChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChessApp.swift
+//  SwiftChess
+//
+//  Created by Daniel Treasure on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
